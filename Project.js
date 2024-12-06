8a88531f5ce7bbb63a68385bac1ca6aeb42b6dde
rssssssssssssssssssssssssssssssssssss <roshinisunjith@gmail.com>
2024-12-06 01:52:17 -0500

    Created duplicate of CS project with 5 files

---
+++ Project.js
@@ -0,0 +1,10 @@
+function cart4(){
+    document.getElementById('order').innerHTML = '<p>Textured Acetate Sheets</p>';
+    console.log('display Textured Acetate Sheets');
+}
+document.getElementById('cart4').addEventListener('click', cart4);
+
+function cart3(){
+    document.getElementById('order').innerText = '<p>Colored Acetate Sheets</p>';
+}
+document.getElementById('cart3').addEventListener('click', cart3);
